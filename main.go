2da01bda6c2004c03192fb98904b50dab3978261
despacitoBear <arsyonoffi@yandex.ru>
2021-02-06 00:29:04 +0300

    Created directry validation

--- main.go
+++ main.go
@@ -8,27 +8,25 @@ import (
 
 //creating temp dir for files
 func main() {
+	//создание папки для временного хранения в проекте
+	CreateTempFolder()
 	var ParentFolderPath string = ""
 	fmt.Println("Enter folder which you want to be ISOed, ZIPed")
 	fmt.Scanf(ParentFolderPath)
-	if os.IsExist(ParentFolderPath) {
-		//smth
+	_, err := os.Stat(ParentFolderPath)
+	if err == nil {
+		fmt.Println("Alredy exists")
 	} else {
-		fmt.Println("Folder doesn't exist, try again")
-		panicfmt.Scanf(ParentFolderPath))
+		fmt.Println("Ready to go")
 	}
-
 }
-func CreateTempFolder() {
+func CreateTempFolder() string {
 	err := os.Mkdir("temp_dir", 0755)
-	if err {
+	if err != nil {
 		log.Fatal(err)
 		panic(err)
+	} else if os.IsExist(err) {
+		return "already exist"
 	}
-
-}
-
-func CompressionZip(string path) string {
-
-	return //path to compressed file
+	return "SomeThing went wrong"
 }
